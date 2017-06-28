1c46337aacd1b3b1ce94f309bc4282b4355b91f3
Gerardo Medina <gerardo.mdn95@gmail.com>
2017-06-28 01:17:45 -0500

    Ya no se consume desde un Json

--- Generador de Gafetes/app/factories/factory.js
+++ Generador de Gafetes/app/factories/factory.js
@@ -6,6 +6,6 @@
         .factory('dataJson', dataJson);
 
     function dataJson($resource) {
-        return $resource("./app/assets/data.json")
+        return $resource("https://script.google.com/macros/s/AKfycbygukdW3tt8sCPcFDlkMnMuNu9bH5fpt7bKV50p2bM/exec?id=1vsf043AXxF9gz9lSaxf46hABz9lSVDQl9hr7dQlSDFI&sheet=responses")
     }
 })();
